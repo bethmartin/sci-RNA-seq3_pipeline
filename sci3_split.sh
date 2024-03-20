2792ca46dd689c54ced1e75b84ab5f8639f64c1e
Beth Martin <martin91@uw.edu>
2024-03-20 16:25:00 -0700

    Update sci3_split.sh
    updating for ubuntu modules and python3

--- sci3_split.sh
+++ sci3_split.sh
@@ -6,8 +6,8 @@ barcode_file=$4
 cutoff=$5
 
 mismatch=1
-python="/net/shendure/vol12/projects/sciRNAseq_script/anaconda2/bin/python2.7"
+# python="/net/shendure/vol12/projects/sciRNAseq_script/anaconda2/bin/python2.7"
 python_script="/net/shendure/vol1/home/martin91/scripts/sciRNAseq3/sam_split.py"
 
-$python $python_script $input_folder/$sample.sam $barcode_file $output_folder $cutoff
+python $python_script $input_folder/$sample.sam $barcode_file $output_folder $cutoff
 echo splitting sample done: $sample
