fd60f1fbf04bd0f026b94624e650ddfeff489eac
Beth Martin <martin91@uw.edu>
2024-03-20 16:22:15 -0700

    Update sci3_trim.sh
    updating for ubuntu modules and python3

--- sci3_trim.sh
+++ sci3_trim.sh
@@ -1,14 +1,12 @@
+#!/usr/bin/bash -l
+
 #added another A so it wouldn't trim my synthetic A-tail 2022-03-07
 
 input_folder=$1
 sample=$2
 output_folder=$3
 
-#had to tweak some modules for centos7, not sure if these will work, but trying out
-module load python/3.7.7 
-module load cutadapt/1.18  
-module load trim_galore/0.6.5 
+
 echo Trimming sample: $sample
 trim_galore $input_folder/$sample*.gz -a AAAAAAAAA --three_prime_clip_R1 1 -o $output_folder
-module unload python/3.7.7
 echo Trimming $sample done.
